9b63b48f92a0a04b0845027417c2495c70ad4fff
Anastasia Aslanova <nastatkachenko53@gmail.com>
2021-10-05 12:15:37 +0300

    new db

--- BD/migrations.sql
+++ BD/migrations.sql
@@ -6,17 +6,14 @@ CREATE TABLE beetroot.products (
        `quantity` INT(6) NOT NULL ,
         `category_id` INT(3) NOT NULL ,
          PRIMARY KEY (`id`));
-
 CREATE TABLE `beetroot`.`category` (
     `name_of_category` VARCHAR(50) NOT NULL ,
     `categoryId` INT(3) NOT NULL , PRIMARY KEY (`categoryId`));
-
 CREATE TABLE beetroot.users (
         `user_id` INT(7) NOT NULL AUTO_INCREMENT ,
         `email` VARCHAR(30) NOT NULL ,
         `password` VARCHAR(30) NOT NULL ,
         PRIMARY KEY (`user_id`));
-
 CREATE TABLE `beetroot`.`shopping_cart` (
         `product_id` INT(7) NOT NULL ,
         `user_id` INT(7),

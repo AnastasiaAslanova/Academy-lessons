bc9f7c1e55e177643387b23c3c7532ef7d6be4b1
Anastasia Aslanova <nastatkachenko53@gmail.com>
2021-10-12 10:32:06 +0300

    exercise 7

--- BD/lesson 20.sql
+++ BD/lesson 20.sql
@@ -3,3 +3,4 @@ SELECT Maker FROM product WHERE type = 'printer' GROUP BY Maker;
 SELECT model,ram,screen FROM laptop WHERE price > 1000;
 SELECT * FROM Printer WHERE color = 'y';
 SELECT model,speed,hd FROM pc WHERE price < 600 AND (cd = '12x' OR cd = '24x');
+

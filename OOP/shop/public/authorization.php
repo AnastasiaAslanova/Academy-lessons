a4ce4c41093d35cf0b2cf4f55f86917cebfe2d22
Anastasia Aslanova <nastatkachenko53@gmail.com>
2021-10-27 23:13:00 +0300

    добавлены логи

--- OOP/shop/public/authorization.php
+++ OOP/shop/public/authorization.php
@@ -1,7 +1,12 @@
 <?php
 use Shop\DB\MySql;
 use Shop\DB\UserRepository;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 require_once __DIR__.'/../vendor/autoload.php';
+
+$startTimer = microtime(true);
+
 $repository = new UserRepository(MySQL::getInstance());
 
 $user = $repository->getUserByEmail($_POST['email']);
@@ -15,3 +20,7 @@ if ($user) {
 }
 
 
+$time = microtime(true) - $startTimer;
+$log = new Logger('log');
+$log->pushHandler(new StreamHandler(__DIR__.'/../logs/test.log', Logger::WARNING));
+$log->warning('index openet',['execution time'=>$time]);
